56ad22c110631a15798261ce8f4398a57b23558c
Joyce Chen <chenjy.mit@gmail.com>
2017-07-26 04:54:48 -0400

    Remove old p2 function with p5

--- drawPhaseDiagram.js
+++ drawPhaseDiagram.js
@@ -1,30 +1,30 @@
-var s;
+
 function drawArrow(w, h, inset, col){
-  s = createShape();
-  s.beginShape();
-  s.fill(col);
-  s.noStroke();
-  s.vertex(0, 0);
-  s.vertex(inset, h/2);
-  s.vertex(0, h);
-  s.vertex(w, h);
-  s.vertex(w + inset, h/2);
-  s.vertex(w, 0);
-  s.endShape(CLOSE);
+  // s = createShape();
+  beginShape();
+  fill(col);
+  noStroke();
+  vertex(0, 0);
+  vertex(inset, h/2);
+  vertex(0, h);
+  vertex(w, h);
+  vertex(w + inset, h/2);
+  vertex(w, 0);
+  endShape(CLOSE);
 }
 
 function drawPhaseDiagram(){
   drawArrow((profilesW + 1.75*MARGIN)/6, (profilesW + 1.75*MARGIN)/12, (profilesW + 1.75*MARGIN)/20, color(100));
   
-  s.setFill(color(100, 100, 100, 150));
-  shape(s, 0.25*MARGIN + profilesX, MARGIN - 10);
-  shape(s, 0.25*MARGIN + profilesX + (profilesW + 1.75*MARGIN)/5, MARGIN - 10);
-  s.setFill(P3);
-  shape(s, 0.25*MARGIN + profilesX + 2*(profilesW + 1.75*MARGIN)/5, MARGIN - 10);
-  s.setFill(color(100, 100, 100, 150));
-  shape(s,0.25*MARGIN + profilesX + 3*(profilesW + 1.75*MARGIN)/5, MARGIN - 10);
-  s.setFill(Launch);
-  shape(s,0.25*MARGIN + profilesX + 4*(profilesW + 1.75*MARGIN)/5, MARGIN - 10);  
+  fill(color(100, 100, 100, 150));
+  translate(0.25*MARGIN + profilesX, MARGIN - 10);
+  translate(0.25*MARGIN + profilesX + (profilesW + 1.75*MARGIN)/5, MARGIN - 10);
+  fill(P3);
+  translate(0.25*MARGIN + profilesX + 2*(profilesW + 1.75*MARGIN)/5, MARGIN - 10);
+  fill(color(100, 100, 100, 150));
+  translate(0.25*MARGIN + profilesX + 3*(profilesW + 1.75*MARGIN)/5, MARGIN - 10);
+  fill(Launch);
+  translate(0.25*MARGIN + profilesX + 4*(profilesW + 1.75*MARGIN)/5, MARGIN - 10);  
 
   fill(255);
   textAlign(CENTER, CENTER);
